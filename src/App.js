968aa5fe6fe8bcb2b4b949924668f3ed62261840
Anuradha <anuradha9920@gmail.com>
2023-06-18 04:21:54 +0530

    my project

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import EthereumTraders from './EthereumTraders';
+import EthereumWalletDataFetcher from './EthereumWalletDataFetcher';
+import BtcTraders from './BtcTraders';
+import BtcUsersDataFetcher from './BtcUsersDataFetcher';
+import GasFees from './GasFees';
+import AvgFeesData from './AvgFeesData';
+import TransactionDataBlockChain from './TransactionDataBlockChain';
+import TransactionData from './TransactionData';
+function App() {
+  return (
+    <div className="app-container">
+      <div className="chart-container">
+        <EthereumTraders />
+      </div>
+      <div className="chart-container">
+          <BtcTraders />
+      </div>
+      <div className="chart-container">
+        <EthereumWalletDataFetcher />
+      </div>
+      <div className="chart-container">
+        <BtcUsersDataFetcher />
+      </div>
+      <div className="chart-container">
+        <GasFees />
+      </div>
+      <div className="chart-container">
+        <AvgFeesData/>
+    </div>
+    <div className="chart-container">
+        <TransactionDataBlockChain />
+    </div>
+    <div className="chart-container">
+    <TransactionData/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
